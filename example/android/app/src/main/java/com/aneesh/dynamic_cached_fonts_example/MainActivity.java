0a5f1c2bb237145749f08fb3263b134e10e6e71d
Abdulkarim <abdulkarim.alnnhas1@gmail.com>
2025-06-26 12:51:46 +0400

    Delete Example

--- example/android/app/src/main/java/com/aneesh/dynamic_cached_fonts_example/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.aneesh.dynamic_cached_fonts_example;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- example/lib/src/components.dart
+++
@@ -1,3 +0,0 @@
-export 'components/display_text.dart';
-export 'components/download_button.dart';
-export 'components/extended_button.dart';
